ea6548f81fa3bfb7aa066071f25ef666b601c2b4
Giovani-Gentil <givijo321@gmail.com>
2024-03-11 13:37:07 -0300

    Aplicando metodo que imprime o tabuleiro

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -9,6 +9,12 @@ namespace JogoXadrez
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8,8);
+            Tela.imprimirTabuleiro(tab);
+
+
+
+
+
             Console.ReadLine();
         }
     }
