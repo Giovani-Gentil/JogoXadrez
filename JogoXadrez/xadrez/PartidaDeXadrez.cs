563c1c9e13c3d27f13f22dc486a950dfcbb8c284
Giovani-Gentil <givijo321@gmail.com>
2024-03-14 23:50:06 -0300

    mudando o nome do metodo movimentoPossivel

--- JogoXadrez/xadrez/PartidaDeXadrez.cs
+++ JogoXadrez/xadrez/PartidaDeXadrez.cs
@@ -84,7 +84,7 @@ namespace JogoXadrez.xadrez {
         }
 
         public void validarPosicaoDeDestino(Posicao origem, Posicao destino) {
-            if (!tab.peca(origem).podeMoverPara(destino)) {
+            if (!tab.peca(origem).movimentoPossivel(destino)) {
                 throw new TabuleiroException("Posição de destino inválida!");
             }
         }
@@ -168,7 +168,7 @@ namespace JogoXadrez.xadrez {
                         {
                             Posicao origem = x.posicao;
                             Posicao destino = new Posicao(i, j);
-                            Peca pecaCapturada = executaMovimento(x.posicao, destino);
+                            Peca pecaCapturada = executaMovimento(origem, destino);
                             bool testeXeque = estaEmXeque(cor);
                             desfazMovimento(origem, destino, pecaCapturada);
                             if (!testeXeque)
